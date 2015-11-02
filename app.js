7b9c68c21c181baf4f10d08c0bb5585e6c789857
Jerebloo <Jerebloo@github.com>
2015-11-02 11:50:47 -0500

    Testing node js and d3 connection

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 
 var http = require('http');
 
@@ -6,14 +6,44 @@ var path = require('path');
 var jade= require('jade');
 
 var db = require('./db');
+ d3 = require('d3')
+
+ */
+var
+    jsdom = require('jsdom');
+
+//    , fs = require('fs')
+  //  , htmlStub = '<html><head></head><body><div id="dataviz-container"></div><script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script></body></html>'
+
+jsdom.env(
+    "<html><body></body></html>",
+    [ 'http://d3js.org/d3.v3.min.js' ],
+    function (err, window) {
+      var svg = window.d3.select("body")
+          .append("svg")
+          .attr("width", 100).attr("height", 100);
+
+      svg.append("rect")
+          .attr("x", 10)
+          .attr("y", 10)
+          .attr("width", 80)
+          .attr("height", 80)
+          .style("fill", "orange");
+// PRINT OUT:
+      console.log(window.d3.select("body").html());
+//  fs.writeFileSync('out.svg', window.d3.select("body").html()); // or this
+    }
+);
+
+/*
 
 var app = express();
 
-//app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
 
-var parsedJSON = require('graph.json');
+//var parsedJSON = require('graph.json');
 
 //app.engine('handlebars', require('express-handlebars')({
 //  defaultLayout: 'layout'
@@ -22,6 +52,8 @@ var parsedJSON = require('graph.json');
 //app.set('view engine', 'handlebars');
 
 //var obj = require("/views/graph.json");
+
+
 
 fs = require('fs');
 
@@ -36,6 +68,9 @@ fs.readFile('./views/force.html', function (err, html) {
     response.end();
   }).listen(3000);
 });
+
+
+
 
 http.createServer(app)
 
@@ -56,3 +91,4 @@ app.get('port')
 }
 
 );
+ */
