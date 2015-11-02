f62dd6675fc6d35d3a041a2016d4a564840e9ef4
Jerebloo <Jerebloo@github.com>
2015-11-02 18:16:35 -0500

    working example for prerendering in node js and d3

--- app.js
+++ app.js
@@ -1,74 +1,78 @@
-/*var express = require('express');
+var express = require('express');
 
 var http = require('http');
 
 var path = require('path');
 var jade= require('jade');
 
+
 var db = require('./db');
- d3 = require('d3')
-
- */
-var
-    jsdom = require('jsdom');
-
-//    , fs = require('fs')
-  //  , htmlStub = '<html><head></head><body><div id="dataviz-container"></div><script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script></body></html>'
-
-jsdom.env(
-    "<html><body></body></html>",
-    [ 'http://d3js.org/d3.v3.min.js' ],
-    function (err, window) {
-      var svg = window.d3.select("body")
-          .append("svg")
-          .attr("width", 100).attr("height", 100);
-
-      svg.append("rect")
-          .attr("x", 10)
-          .attr("y", 10)
-          .attr("width", 80)
-          .attr("height", 80)
-          .style("fill", "orange");
-// PRINT OUT:
-      console.log(window.d3.select("body").html());
-//  fs.writeFileSync('out.svg', window.d3.select("body").html()); // or this
-    }
-);
-
-/*
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-
 app.set('views', path.join(__dirname, 'views'));
 
-//var parsedJSON = require('graph.json');
-
-//app.engine('handlebars', require('express-handlebars')({
-//  defaultLayout: 'layout'
-//}));
-
-//app.set('view engine', 'handlebars');
-
-//var obj = require("/views/graph.json");
 
 
+var d3 = require('d3')
+    , jsdom = require('jsdom')
+    , fs = require('fs')
+    , htmlStub = '<html><head></head><body><div id="dataviz-container"></div><script src="js/d3.v3.min.js"></script></body></html>'
 
-fs = require('fs');
+jsdom.env({
+    features : { QuerySelector : true }
+    , html : htmlStub
+    , done : function(errors, window) {
+        // this callback function pre-renders the dataviz inside the html document, then export result into a static file
 
 
-fs.readFile('./views/force.html', function (err, html) {
-  if (err) {
-    throw err;
-  }
-  http.createServer(function(request, response) {
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write(html);
-    response.end();
-  }).listen(3000);
+
+        var el = window.document.querySelector('#dataviz-container')
+            , body = window.document.querySelector('body')
+            , circleId = 'a2324'  // say, this value was dynamically retrieved from some database
+
+        // generate the dataviz
+        d3.select(el)
+            .append('svg:svg')
+            .attr('width', 600)
+            .attr('height', 300)
+            .append('circle')
+            .attr('cx', 300)
+            .attr('cy', 150)
+            .attr('r', 30)
+            .attr('fill', '#26963c')
+            .attr('id', circleId) // say, this value was dynamically retrieved from some database
+
+
+
+
+        // make the client-side script manipulate the circle at client side)
+        var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
+
+        d3.select(body)
+            .append('script')
+            .html(clientScript)
+
+        // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
+        var svgsrc = window.document.documentElement.innerHTML
+        fs.writeFile('views/index.html', svgsrc, function(err) {
+            if(err) {
+                console.log('error saving document', err)
+            } else {
+                console.log('The file was saved!')
+            }
+        })
+    } // end jsDom done callback
+})
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    res.render('index.html', {});
 });
 
+app.set('port', process.env.PORT || 3000);
 
 
 
@@ -91,4 +95,4 @@ app.get('port')
 }
 
 );
- */
+
